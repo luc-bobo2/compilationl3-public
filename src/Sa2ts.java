9d693d8ebe62d19fa862e00467dfbf5343f745f6
baptistegasser <baptistegasser36@gmail.com>
2020-02-17 18:31:20 +0100

    Modif de la classe.

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -3,9 +3,11 @@ import ts.Ts;
 import ts.TsItemVar;
 
 public class Sa2ts extends SaDepthFirstVisitor <Void> {
+    private int depth;
     private Ts tableGlobale;
 
     public Sa2ts (SaNode saRoot) {
+        this.depth = 0;
         this.tableGlobale = new Ts();
         saRoot.accept(this);
     }
@@ -15,8 +17,19 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
     }
 
     @Override
+    public void defaultIn(SaNode node) {
+        System.out.printf("Visiting | depth: %03d | node type: %s.%n", depth, node.getClass().getCanonicalName());
+        this.depth += 1;
+    }
+
+    @Override
+    public void defaultOut(SaNode node) {
+        System.out.printf("Exiting  | depth: %03d | node type: %s.%n", depth, node.getClass().getCanonicalName());
+        this.depth -= 1;
+    }
+
+    @Override
     public Void visit(SaDecVar node) {
-        System.out.println("hey from visit(SaDecVar node);");
         defaultIn(node);
         this.tableGlobale.addVar(node.getNom(), 1);
         defaultOut(node);
@@ -25,7 +38,6 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
 
     @Override
     public Void visit(SaDecTab node) {
-        System.out.println("hey from visit(SaDecTab node);");
         defaultIn(node);
         this.tableGlobale.addVar(node.getNom(), node.getTaille());
         defaultOut(node);
@@ -34,62 +46,70 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
 
     @Override
     public Void visit(SaDecFonc node) {
-        try {
-            System.out.println("hey from visit(SaDecFonc node);");
-            defaultIn(node);
+        defaultIn(node);
 
-            // Declare the local table
-            Ts tableLocale = new Ts();
-            // Store the number of parameters, default to 0
-            int nbParams = 0;
+        // Save the global table and replace it with a new table local to the function
+        Ts saveTable = this.tableGlobale;
+        this.tableGlobale = new Ts();
 
-            // Adding parameters to the local table
-            if (node.getParametres() != null) {
-                // Update the parameters count
-                nbParams = node.getParametres().length();
-                Ts parametersTable = new Sa2ts(node.getParametres()).tableGlobale;
-                for(TsItemVar var : parametersTable.variables.values()) {
-                    tableLocale.addParam(var.identif);
-                }
+        // Store the number of parameters, default to 0
+        int nbParams = 0;
+
+        // Adding parameters to the local table
+        if (node.getParametres() != null) {
+            // Update the parameters count
+            nbParams = node.getParametres().length();
+            Ts parametersTable = new Sa2ts(node.getParametres()).tableGlobale;
+            for(TsItemVar var : parametersTable.variables.values()) {
+                this.tableGlobale.addParam(var.identif);
             }
-
-            // Adding variable to the local table
-            if (node.getVariable() != null) {
-                Ts tableVariable = new Sa2ts(node.getVariable()).tableGlobale;
-                for(TsItemVar var : tableVariable.variables.values()) {
-                    tableLocale.addVar(var.identif, var.taille);
-                }
-            }
-
-            // Finally add the function to the table
-            this.tableGlobale.addFct(node.getNom(), nbParams, tableLocale, node);
-            defaultOut(node);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
+        // Adding variable to the local table
+        if (node.getVariable() != null) {
+            node.getVariable().accept(this);
+        }
+
+        // Visit the function body
+        node.getCorps().accept(this);
+
+        // Restore the global table
+        Ts tableLocale = this.tableGlobale;
+        this.tableGlobale = saveTable;
+
+        // Add the function to the table
+        this.tableGlobale.addFct(node.getNom(), nbParams, tableLocale, node);
+
+        defaultOut(node);
         return null;
     }
 
     @Override
     public Void visit(SaVarSimple node) {
-        System.out.println("hey from visit(SaVarSimple node);");
         defaultIn(node);
+        if (this.tableGlobale.getVar(node.getNom()) == null) {
+            throw new CompilerException(CompilerError.CALL_TO_UNDEFINED_VARIABLE);
+        }
         defaultOut(node);
         return null;
     }
 
     @Override
     public Void visit(SaVarIndicee node) {
-        System.out.println("hey from visit(SaVarIndicee node);");
         defaultIn(node);
+        if (this.tableGlobale.getVar(node.getNom()) == null) {
+            throw new CompilerException(CompilerError.CALL_TO_UNDEFINED_VARIABLE);
+        }
         defaultOut(node);
         return null;
     }
 
     @Override
     public Void visit(SaAppel node) {
-        System.out.println("hey from visit(SaAppel node);");
         defaultIn(node);
+        if (this.tableGlobale.getFct(node.getNom()) == null) {
+            throw new CompilerException(CompilerError.CALL_TO_UNDEFINED_FUNCTION);
+        }
         defaultOut(node);
         return null;
     }
