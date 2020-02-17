6469f0a52f0979efe08e953fb65f5775bff09c19
baptistegasser <baptistegasser36@gmail.com>
2020-02-17 18:20:13 +0100

    Réindentation du code.

--- src/Compiler.java
+++ src/Compiler.java
@@ -3,48 +3,46 @@ import sa.SaNode;
 import sc.parser.*;
 import sc.lexer.*;
 import sc.node.*;
+
 import java.io.*;
+
 import ts.*;
 //import c3a.*;
 //import nasm.*;
 //import fg.*;
 
-public class Compiler
-{
-    public static void main(String[] args)
-    {
-	PushbackReader br = null;
-	String baseName = null;
-	try {
-	    if (0 < args.length) {
-		br = new PushbackReader(new FileReader(args[0]));
-		baseName = removeSuffix(args[0], ".l");
-	    }
-	    else{
-		System.out.println("il manque un argument");
-	    }
-	}
-	catch (IOException e) {
-	    e.printStackTrace();
-	}
-	try {
-	    // Create a Parser instance.
-	    Parser p = new Parser(new Lexer(br));
-	    // Parse the input.
-	    Start tree = p.parse();
+public class Compiler {
+    public static void main(String[] args) {
+        PushbackReader br = null;
+        String baseName = null;
+        try {
+            if (0 < args.length) {
+                br = new PushbackReader(new FileReader(args[0]));
+                baseName = removeSuffix(args[0], ".l");
+            } else {
+                System.out.println("il manque un argument");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            // Create a Parser instance.
+            Parser p = new Parser(new Lexer(br));
+            // Parse the input.
+            Start tree = p.parse();
 
-	    System.out.println("[SC]");
-	    tree.apply(new Sc2Xml(baseName));
+            System.out.println("[SC]");
+            tree.apply(new Sc2Xml(baseName));
 
-	    System.out.println("[SA]");
-	    Sc2sa sc2sa = new Sc2sa();
-	    tree.apply(sc2sa);
-	    SaNode saRoot = sc2sa.getRoot();
-	    new Sa2Xml(saRoot, baseName);
+            System.out.println("[SA]");
+            Sc2sa sc2sa = new Sc2sa();
+            tree.apply(sc2sa);
+            SaNode saRoot = sc2sa.getRoot();
+            new Sa2Xml(saRoot, baseName);
 
-	    System.out.println("[TABLE SYMBOLES]");
-	    Ts table = new Sa2ts(saRoot).getTableGlobale();
-	    table.afficheTout(baseName);
+            System.out.println("[TABLE SYMBOLES]");
+            Ts table = new Sa2ts(saRoot).getTableGlobale();
+            table.afficheTout(baseName);
 
 	    /*
 	    System.out.println("[C3A]");
@@ -64,23 +62,20 @@ public class Compiler
 	    fgSolution.affiche(baseName);*/
 
 
-
-	}
-	catch(Exception e){
-		// Un message est pas suffisant bordel
-		// 45min perdu à cause d'un NullPointerException et que le seul message c'est "null", ça aide pas putain
-	    System.out.println(e.getMessage());
-	    e.printStackTrace();
-	}
+        } catch (Exception e) {
+            // Un message est pas suffisant bordel
+            // 45min perdu à cause d'un NullPointerException et que le seul message c'est "null", ça aide pas putain
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 
-    public static String removeSuffix(final String s, final String suffix)
-    {
-	if (s != null && suffix != null && s.endsWith(suffix)){
-	    return s.substring(0, s.length() - suffix.length());
-	}
-	return s;
+    public static String removeSuffix(final String s, final String suffix) {
+        if (s != null && suffix != null && s.endsWith(suffix)) {
+            return s.substring(0, s.length() - suffix.length());
+        }
+        return s;
     }
 
 }
