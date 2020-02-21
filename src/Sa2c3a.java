5e11ff632bd588664779252b17bcf74d69b9172a
baptistegasser <baptistegasser36@gmail.com>
2020-02-21 18:25:12 +0100

    Ajout du squelette

--- src/Sa2c3a.java
+++ src/Sa2c3a.java
@@ -14,22 +14,157 @@ public class Sa2c3a  extends SaDepthFirstVisitor<C3aOperand> {
     }
 
     public C3a getC3a() {
-
         return c3a;
     }
 
+    @Override
+    public C3aOperand visit(SaProg node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaDecTab node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExp node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpInt node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpVar node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaInstEcriture node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaInstTantQue node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaLInst node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
     public C3aOperand visit(SaDecVar node) {
-        return new C3aVar(node.tsItem,c3a.);
+        throw new UnsupportedOperationException(); // TODO
     }
 
-    public C3aOperand visit(SaDecTab node){
+    @Override
+    public C3aOperand visit(SaInstAffect node) {
+        throw new UnsupportedOperationException(); // TODO
     }
 
-    public C3aOperand visit(SaDecFonc node) {
-       // node.accept(this);
-        return new C3aFunction(node.tsItem);
+    @Override
+    public C3aOperand visit(SaLDec node) {
+        throw new UnsupportedOperationException(); // TODO
     }
 
+    @Override
+    public C3aOperand visit(SaAppel node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpAppel node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpAdd node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
     public C3aOperand visit(SaExpSub node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpMult node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpDiv node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpInf node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpEqual node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpAnd node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpOr node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpNot node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaExpLire node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaInstBloc node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaInstSi node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaInstRetour node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaLExp node) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public C3aOperand visit(SaDecFonc node) {
+        return new C3aFunction(table.getFct(node.getNom()));
+    }
+
+    @Override
+    public C3aOperand visit(SaVarIndicee node) {
+        C3aOperand op = node.getIndice().accept(this);
+        return new C3aVar(table.getVar(node.getNom()), op);
+    }
+
+    @Override
+    public C3aOperand visit(SaVarSimple node) {
+        return new C3aVar(table.getVar(node.getNom()), null);
     }
 }
