234dd74d732894c0dc7ce37309c53548a923a590
baptistegasser <baptistegasser36@gmail.com>
2020-02-21 18:07:15 +0100

    Modification des exceptions

--- src/CompilerException.java
+++ src/CompilerException.java
@@ -10,15 +10,27 @@ public class CompilerException extends RuntimeException {
 
     public void printDescription(PrintStream out) {
         switch (type) {
-            case CALL_TO_UNDEFINED_VAR:
-                out.println("[COMPILATION ERROR] Trying to reference an undefined variable.");
+            case VAR_ALREADY_DECLARED:
+                out.println("[COMPILATION ERROR] Trying to declare an already declared var.");
                 break;
-            case CALL_TO_UNDEFINED_VAR_INDICEE:
+            case CALL_TO_UNDEFINED_VAR:
+                out.println("[COMPILATION ERROR] Trying to reference an undefined var.");
+                break;
+
+            case TAB_ALREADY_DECLARED:
+                out.println("[COMPILATION ERROR] Trying to declare an already declared array.");
+                break;
+            case CALL_TO_UNDEFINED_TAB:
                 out.println("[COMPILATION ERROR] Trying to reference an undefined array.");
+                break;
+
+            case FUNCTION_ALREADY_DECLARED:
+                out.println("[COMPILATION ERROR] Trying to declare an already declared function.");
                 break;
             case CALL_TO_UNDEFINED_FUNCTION:
                 out.println("[COMPILATION ERROR] Trying to reference an undefined function.");
                 break;
+
             default:
                 out.println("[COMPILATION ERROR] Unknown compile exception.");
         }
