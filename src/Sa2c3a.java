ad3f4343457c876b501bf662d9dea20234d06e58
lucco <luc.bobo@etu.univ-amu.fr>
2020-02-21 17:22:18 +0100

    a

--- src/Sa2c3a.java
+++ src/Sa2c3a.java
@@ -1,27 +1,28 @@
 import c3a.C3a;
+import c3a.C3aInstAdd;
 import c3a.C3aOperand;
+import sa.SaDecVar;
 import sa.SaDepthFirstVisitor;
 import sa.SaNode;
 import sa.SaProg;
 import ts.Ts;
 
 public class Sa2c3a  extends SaDepthFirstVisitor<C3aOperand> {
-/*    SaNode saRoot;
-    Ts table;*/
+
+    Ts table;
     private C3a c3a;
 
-    public Sa2c3a (SaNode saRoot) {
+    public Sa2c3a (SaNode saRoot, Ts table) {
         c3a = new C3a();
-/*        this.saRoot = saRoot;
-        this.table = table;*/
         saRoot.accept(this);
+        this.table = table;
     }
 
     public C3a getC3a() {
         return c3a;
     }
 
-    public C3aOperand visit(SaProg node) {
-
+    public C3aOperand visit(SaDecVar node) {
+        return node
     }
 }
