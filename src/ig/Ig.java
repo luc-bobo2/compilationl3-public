efd6b6dfeb02651783ea9742a20ffa936dbbae15
baptistegasser <baptistegasser36@gmail.com>
2020-04-06 11:26:24 +0200

    Nettoyage du code en prévision du travail sur le dernier tp

--- src/ig/Ig.java
+++ src/ig/Ig.java
@@ -1,11 +1,13 @@
 package ig;
 
-import fg.*;
-import nasm.*;
-import util.graph.*;
-import util.intset.*;
-import java.util.*;
-import java.io.*;
+import fg.FgSolution;
+import nasm.Nasm;
+import util.graph.Graph;
+import util.graph.Node;
+import util.graph.NodeList;
+
+import java.io.IOException;
+import java.io.PrintStream;
 
 public class Ig {
     public Graph graph;
@@ -14,58 +16,48 @@ public class Ig {
     public Nasm nasm;
     public Node int2Node[];
 
-    
-    public Ig(FgSolution fgs){
-	this.fgs = fgs;
- 	this.graph = new Graph();
-	this.nasm = fgs.nasm;
-	this.regNb = this.nasm.getTempCounter();
-	this.int2Node = new Node[regNb];
-	//this.build();
+    public Ig(FgSolution fgs) {
+        this.fgs = fgs;
+        this.graph = new Graph();
+        this.nasm = fgs.nasm;
+        this.regNb = this.nasm.getTempCounter();
+        this.int2Node = new Node[regNb];
+        this.build();
     }
 
-    public void construction(){
+    public void build() {
+
     }
 
-    public int[] getPrecoloredTemporaries()
-    {
-    	return null;
+    public int[] getPrecoloredTemporaries() {
+        return null;
     }
 
-
-    public void allocateRegisters(){
+    public void allocateRegisters() {
     }
 
+    public void affiche(String baseFileName) {
+        String fileName;
+        PrintStream out = System.out;
 
-    public void affiche(String baseFileName){
-	String fileName;
-	PrintStream out = System.out;
-	
-	if (baseFileName != null){
-	    try {
-		baseFileName = baseFileName;
-		fileName = baseFileName + ".ig";
-		out = new PrintStream(fileName);
-	    }
-	    
-	    catch (IOException e) {
-		System.err.println("Error: " + e.getMessage());
-	    }
-	}
-	
-	for(int i = 0; i < regNb; i++){
-	    Node n = this.int2Node[i];
-	    out.print(n + " : ( ");
-	    for(NodeList q=n.succ(); q!=null; q=q.tail) {
-		out.print(q.head.toString());
-		out.print(" ");
-	    }
-	    out.println(")");
-	}
+        if (baseFileName != null) {
+            try {
+                baseFileName = baseFileName;
+                fileName = baseFileName + ".ig";
+                out = new PrintStream(fileName);
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+
+        for (int i = 0; i < regNb; i++) {
+            Node n = this.int2Node[i];
+            out.print(n + " : ( ");
+            for (NodeList q = n.succ(); q != null; q = q.tail) {
+                out.print(q.head.toString());
+                out.print(" ");
+            }
+            out.println(")");
+        }
     }
 }
-	    
-    
-
-    
-    
--- src/util/graph/ColorGraph.java
+++ src/util/graph/ColorGraph.java
@@ -1,112 +1,92 @@
 package util.graph;
 
-import util.graph.*;
-import util.intset.*;
-import java.util.*;
-import java.io.*;
+import util.intset.IntSet;
+
+import java.util.Stack;
 
 public class ColorGraph {
-    public  Graph          G;
-    public  int            R;
-    public  int            K;
+    public Graph G;                 // Le graphe a colorer
+    public int R;                   // Nombre de sommets
+    public int K;                   // Nombre de couleurs
     private Stack<Integer> pile;
-    public  IntSet         enleves;
-    public  IntSet         deborde;
-    public  int[]          couleur;
-    public  Node[]         int2Node;
-    static  int            NOCOLOR = -1;
+    public IntSet enleves;          // sommets enlevés
+    public IntSet deborde;          // sommets qui débordent
+    public int[] couleur;           // Tableau de couleurs
+    public Node[] int2Node;         // Accès rapide au nodes
+    static int NOCOLOR = -1;
 
-    public ColorGraph(Graph G, int K, int[] phi){
-	this.G       = G;
-	this.K       = K;
-	pile         = new Stack<Integer>(); 
-	R            = G.nodeCount();
-	couleur      = new int[R];
-	//removed      = new IntSet(R);
-	//spill        = new IntSet(R);
-	int2Node     = G.nodeArray();
-	for(int v=0; v < R; v++){
-	    int preColor = phi[v];
-	    if(preColor >= 0 && preColor < K)
-		couleur[v] = phi[v];
-	    else
-		couleur[v] = NOCOLOR;
-	}
+    public ColorGraph(Graph G, int K, int[] phi) {
+        this.G = G;
+        this.K = K;
+        pile = new Stack<Integer>();
+        R = G.nodeCount();
+        couleur = new int[R];
+        enleves = new IntSet(R);
+        deborde = new IntSet(R);
+        int2Node = G.nodeArray();
+        for (int v = 0; v < R; v++) {
+            int preColor = phi[v];
+            if (preColor >= 0 && preColor < K)
+                couleur[v] = phi[v];
+            else
+                couleur[v] = NOCOLOR;
+        }
     }
 
-    /*-------------------------------------------------------------------------------------------------------------*/
-    /* associe une couleur à tous les sommets se trouvant dans la pile */
-    /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public void selection()
-    {
+    /**
+     * associe une couleur à tous les sommets se trouvant dans la pile
+     */
+    public void selection() {
     }
-    
-    /*-------------------------------------------------------------------------------------------------------------*/
-    /* récupère les couleurs des voisins de t */
-    /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public IntSet couleursVoisins(int t)
-    {
+
+    /**
+     * récupère les couleurs des voisins de t
+     * @param t Le noeud t
+     * @return Les couleurs des voisins
+     */
+    public IntSet couleursVoisins(int t) {
         return null;
     }
-    
-    /*-------------------------------------------------------------------------------------------------------------*/
-    /* recherche une couleur absente de colorSet */
-    /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public int choisisCouleur(IntSet colorSet)
-    {
-        return 0;
-    }
-    
-    /*-------------------------------------------------------------------------------------------------------------*/
-    /* calcule le nombre de voisins du sommet t */
-    /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public int nbVoisins(int t)
-    {
+
+    /**
+     * recherche une couleur absente de colorSet
+     * @param colorSet
+     * @return
+     */
+    public int choisisCouleur(IntSet colorSet) {
         return 0;
     }
 
-    /*-------------------------------------------------------------------------------------------------------------*/
-    /* simplifie le graphe d'interférence g                                                                        */
-    /* la simplification consiste à enlever du graphe les temporaires qui ont moins de k voisins                   */
-    /* et à les mettre dans une pile                                                                               */
-    /* à la fin du processus, le graphe peut ne pas être vide, il s'agit des temporaires qui ont au moins k voisin */
-    /*-------------------------------------------------------------------------------------------------------------*/
-
-    public int simplification()
-    {
+    /**
+     * calcule le nombre de voisins du sommet t
+     */
+    public int nbVoisins(int t) {
         return 0;
     }
-    
-    /*-------------------------------------------------------------------------------------------------------------*/
-    /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public void debordement()
-    {
+
+    /**
+     * simplifie le graphe d'interférence g
+     * la simplification consiste à enlever du graphe les temporaires qui ont moins de k voisins
+     * et à les mettre dans une pile
+     * à la fin du processus, le graphe peut ne pas être vide, il s'agit des temporaires qui ont au moins k voisin
+     */
+    public int simplification() {
+        return 0;
     }
 
-
-    /*-------------------------------------------------------------------------------------------------------------*/
-    /*-------------------------------------------------------------------------------------------------------------*/
-
-    public void coloration()
-    {
-	this.simplification();
-	this.debordement();
-	this.selection();
+    public void debordement() {
     }
 
-    void affiche()
-    {
-	System.out.println("vertex\tcolor");
-	for(int i = 0; i < R; i++){
-	    System.out.println(i + "\t" + couleur[i]);
-	}
+    public void coloration() {
+        this.simplification();
+        this.debordement();
+        this.selection();
     }
-    
-    
 
+    void affiche() {
+        System.out.println("vertex\tcolor");
+        for (int i = 0; i < R; i++) {
+            System.out.println(i + "\t" + couleur[i]);
+        }
+    }
 }
